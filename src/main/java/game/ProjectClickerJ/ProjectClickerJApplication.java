e1e7de06bd37ce425f43051f47f43990fa44be6b
axelvuillaume <axelvuillaume@hotmail.fr>
2023-10-13 16:04:54 +0200

    defaultReq

---
+++ src/main/java/game/ProjectClickerJ/ProjectClickerJApplication.java
@@ -0,0 +1,13 @@
+package game.ProjectClickerJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectClickerJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectClickerJApplication.class, args);
+	}
+
+}
---
+++ src/test/java/game/ProjectClickerJ/ProjectClickerJApplicationTests.java
@@ -0,0 +1,13 @@
+package game.ProjectClickerJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectClickerJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
