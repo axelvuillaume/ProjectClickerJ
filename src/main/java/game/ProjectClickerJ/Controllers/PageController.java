426dbc1e38d054bfb9b6ce8534b7ec5d8c10cf88
liro-u <86537787+liro-u@users.noreply.github.com>
2023-10-24 14:05:03 +0200

    controller redirection + component sidebar

--- src/main/java/game/ProjectClickerJ/Controllers/PageController.java
+++ src/main/java/game/ProjectClickerJ/Controllers/PageController.java
@@ -1,23 +1,35 @@
 package game.ProjectClickerJ.Controllers;
 
+import game.ProjectClickerJ.ObjectRepositories.PlayerRepository;
+import game.ProjectClickerJ.Utils.Utils;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+    @Autowired
+    PlayerRepository playerRepository;
     @GetMapping("/connexion")
-    public String getConnexionPage(Model model) {
+    public String getConnexionPage(Model model, HttpSession session) {
+        if (!Utils.IsNotLogin(session,  playerRepository)) {
+            return "index";
+        }
         return "connexion";
     }
 
     @GetMapping("/index")
-    public String getIndexPage(Model model) {
+    public String getIndexPage(Model model, HttpSession session) {
+        if (Utils.IsNotLogin(session,  playerRepository)) {
+            return "connexion";
+        }
         return "index";
     }
 
     @GetMapping("/test")
-    public String getTestPage(Model model) {
+    public String getTestPage(Model model, HttpSession session) {
         return "testComponent";
     }
 }
